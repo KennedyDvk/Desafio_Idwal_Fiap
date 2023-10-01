d2f7e07810b5c871c69f35d42ffa215f0d17255b
Kennedy Cesar Pimentel <104803647+KennedyDvk@users.noreply.github.com>
2023-10-01 11:50:35 -0300

    adcionei a função Fetch

--- Frontend/main.js
+++ Frontend/main.js
@@ -4,8 +4,75 @@ var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
+        // Faça uma requisição HTTP GET para a sua API de backend
+        fetch('http://localhost:8080/wanteds', {
+            method: 'GET',
+            // Adicione qualquer cabeçalho necessário aqui (por exemplo, content-type)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Manipule os dados da resposta e atualize seu HTML
+            let dadosProcurados = data; // Ajuste isso com base na estrutura da resposta da sua API
+            atualizarUI(dadosProcurados);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
 
-fetch("https://dummyjson.com/user/1")
+        // Função para atualizar a interface do usuário com dados do backend
+        function atualizarUI(data) {
+            // Modifique os elementos HTML com os dados recebidos
+            let tabela = document.getElementById('tabela');
+            tabela.innerHTML = `
+                <p>Nome: ${data.nome}</p>
+                <p>ID: ${data.id}</p>
+                <p>Altura: ${data.altura}</p>
+                <p>Peso: ${data.peso}</p>
+                <p>Data de Nascimento: ${data.dataNascimento}</p>
+                
+            `;
+        }
+
+
+  /*fetch("http://localhost:8080?wanted_origin_id") // Use o URL correto do seu endpoint
+    .then(response => response.json())
+    .then(data => {
+        const tabela = document.getElementById('tabela');
+        const dados = data; // A resposta JSON contém diretamente os dados desejados
+
+        // Crie uma tabela HTML para exibir os dados
+        const table = document.createElement('table');
+        table.classList.add('table'); // Adicione classes de estilo conforme necessário
+
+        // Crie a primeira linha da tabela com cabeçalhos
+        const headerRow = table.insertRow(0);
+        const headers = Object.keys(dados[0]); // Assumindo que todos os objetos têm a mesma estrutura
+
+        headers.forEach(header => {
+            const th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+        });
+
+        // Preencha a tabela com os dados
+        dados.forEach(item => {
+            const row = table.insertRow();
+            headers.forEach(header => {
+                const cell = row.insertCell();
+                cell.textContent = item[header];
+            });
+        });
+
+        // Limpe o conteúdo anterior da tabela e adicione a nova tabela
+        tabela.innerHTML = '';
+        tabela.appendChild(table);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar itens:', error);
+    });*/
+
+
+/*fetch()
     .then(response => response.json())
     .then(result => console.log(result))
     .then(function(data) {
@@ -14,5 +81,8 @@ fetch("https://dummyjson.com/user/1")
       })
       .catch(function(error) {
         console.log(error);
-      });
+      });*/
+
+
+     
     
